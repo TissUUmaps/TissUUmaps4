d6c8692c557bdb94d22d27b9228a24556cc34307
Jonas Windhager <jonas.windhager@scilifelab.se>
2024-08-21 15:51:24 +0200

    add TissUUmaps HTML

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.tsx";
+import "./bootstrap.scss";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
