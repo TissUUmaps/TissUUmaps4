beef0a9f8add7251babe2fd6d8287f57c44bcd1c
Jonas Windhager <jonas.windhager@scilifelab.se>
2024-08-28 23:04:09 +0200

    update layer properties

--- src/model/layer.ts
+++ src/model/layer.ts
@@ -1,12 +1,12 @@
 export default interface Layer {
   name: string; // image name
   tileSource: string; // image URL
-  x?: number; // x-position in pixels
-  y?: number; // y-position in pixels
-  rotation?: number; // rotation in degrees
-  flipX?: boolean; // flip horizontally
-  scale?: number; // factor for converting pixels to viewport coordinates
-  opacity?: number; // alpha value
-  visible?: boolean; // visibility
+  x: number; // x-position in pixels
+  y: number; // y-position in pixels
+  rotation: number; // rotation in degrees
+  flip: boolean; // flip horizontally
+  scale: number; // factor for converting pixels to viewport coordinates
+  opacity: number; // alpha value
+  visible: boolean; // visibility
   // TODO clip {x,y,w,h}
 }
--- src/utils/OpenSeadragonUtils.ts
+++ src/utils/OpenSeadragonUtils.ts
@@ -20,34 +20,78 @@ export default class OpenSeadragonUtils {
     );
     for (const layer of layersToRemove) {
       const index = oldLayers.indexOf(layer);
-      const item = viewer.world.getItemAt(index);
-      viewer.world.removeItem(item);
+      OpenSeadragonUtils.removeLayer(viewer, index);
     }
-    // sort existing layers
+    // sort and update existing layers
     const oldLayerOrder = oldLayers.filter((layer) =>
       newLayers.includes(layer),
     );
     const newLayerOrder = newLayers.filter((layer) =>
       oldLayers.includes(layer),
     );
-    for (const [oldIndex, layer] of oldLayerOrder.entries()) {
-      const newIndex = newLayerOrder.indexOf(layer);
+    for (const [newIndex, layer] of newLayerOrder.entries()) {
+      const oldIndex = oldLayerOrder.indexOf(layer);
       if (oldIndex !== newIndex) {
-        const tiledImage = viewer.world.getItemAt(oldIndex);
-        viewer.world.setItemIndex(tiledImage, newIndex);
+        OpenSeadragonUtils.moveLayer(viewer, oldIndex, newIndex);
       }
+      OpenSeadragonUtils.updateLayer(viewer, newIndex, layer);
     }
     // insert new layers
-    const layersToInsert = newLayers.filter(
-      (layer) => !oldLayers.includes(layer),
-    );
-    for (const layer of layersToInsert) {
+    const layersToAdd = newLayers.filter((layer) => !oldLayers.includes(layer));
+    for (const layer of layersToAdd) {
       const index = newLayers.indexOf(layer);
-      viewer.addTiledImage({
-        tileSource: layer.tileSource,
-        index: index,
-        // TODO addTiledImage options
-      });
+      OpenSeadragonUtils.addLayer(viewer, layer, index);
     }
+  }
+
+  static updateLayer(
+    viewer: OpenSeadragon.Viewer,
+    index: number,
+    layer: Layer,
+  ): void {
+    const tiledImage = viewer.world.getItemAt(index);
+    // TODO tileSource
+    // TODO x, y
+    if (layer.rotation !== tiledImage.getRotation()) {
+      tiledImage.setRotation(layer.rotation);
+    }
+    if (layer.flip !== tiledImage.getFlip()) {
+      tiledImage.setFlip(layer.flip);
+    }
+    // TODO scale
+    if (layer.visible && layer.opacity !== tiledImage.getOpacity()) {
+      tiledImage.setOpacity(layer.opacity);
+    } else if (!layer.visible && tiledImage.getOpacity() !== 0) {
+      tiledImage.setOpacity(0);
+    }
+  }
+
+  private static addLayer(
+    viewer: OpenSeadragon.Viewer,
+    layer: Layer,
+    index?: number,
+  ): void {
+    viewer.addTiledImage({
+      tileSource: layer.tileSource,
+      index: index,
+      // TODO addTiledImage options
+    });
+  }
+
+  private static moveLayer(
+    viewer: OpenSeadragon.Viewer,
+    oldIndex: number,
+    newIndex: number,
+  ): void {
+    const tiledImage = viewer.world.getItemAt(oldIndex);
+    viewer.world.setItemIndex(tiledImage, newIndex);
+  }
+
+  private static removeLayer(
+    viewer: OpenSeadragon.Viewer,
+    index: number,
+  ): void {
+    const tiledImage = viewer.world.getItemAt(index);
+    viewer.world.removeItem(tiledImage);
   }
 }
