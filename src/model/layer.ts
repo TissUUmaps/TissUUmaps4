4f73fb7e8ea4654da9d55a23196a64a04c9794ad
Jonas Windhager <jonas.windhager@scilifelab.se>
2024-09-06 16:45:27 +0200

    layer model

--- src/model/layer.ts
+++ src/model/layer.ts
@@ -1,10 +1,26 @@
 import Image from "./image";
 import Shapes from "./shapes";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface LayerSettings {}
+export interface LayerSettings {
+  /** layer origin in world coordinates */
+  origin: { x: number; y: number };
 
-export const defaultLayerSettings: LayerSettings = {};
+  /** layer scale, translating layer coordinates (e.g., pixels) to world coordinates */
+  scale: number;
+
+  /** reflection along the x axis */
+  flipx: boolean;
+
+  /** rotation angle, in degrees */
+  rotation: number;
+}
+
+export const defaultLayerSettings: LayerSettings = {
+  origin: { x: 0, y: 0 },
+  scale: 1,
+  flipx: false,
+  rotation: 0,
+};
 
 export interface Layer {
   images: Image[];
