6f2ea57f80c131ad693c8bcc5d81c5d5c7c399f4
Jonas Windhager <jonas.windhager@scilifelab.se>
2024-09-18 20:26:02 +0200

    prepare viewer update

--- src/utils/OpenSeadragonUtils.ts
+++ src/utils/OpenSeadragonUtils.ts
@@ -3,11 +3,13 @@ import OpenSeadragon from "openseadragon";
 import Layer from "../model/layer";
 
 export type ViewerState = {
-  [layerId: string]: {
-    dummyTiledImageIndex: number;
-    dirty: boolean;
-    images: {
-      [imageId: string]: { tiledImageIndex: number; dirty: boolean };
+  layers: {
+    [layerId: string]: {
+      dummyTiledImageIndex: number;
+      dirty: boolean;
+      images: {
+        [imageId: string]: { tiledImageIndex: number; dirty: boolean };
+      };
     };
   };
 };
@@ -16,17 +18,51 @@ export default class OpenSeadragonUtils {
   static createViewer(viewerElement: HTMLDivElement): OpenSeadragon.Viewer {
     return new OpenSeadragon.Viewer({
       element: viewerElement,
-      // TODO OpenSeadragon options
+      // TODO OpenSeadragon viewer options
     });
   }
 
   static updateViewer(
     viewer: OpenSeadragon.Viewer,
     viewerState: ViewerState,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     layers: { [layerId: string]: Layer },
   ): ViewerState {
-    // TODO update viewer according to the desired viewerState
+    for (const layerId of Object.keys(viewerState.layers)) {
+      for (const imageId of Object.keys(viewerState.layers[layerId].images)) {
+        if (
+          !(layerId in layers) ||
+          viewerState.layers[layerId].dirty ||
+          !(imageId in layers[layerId].images) ||
+          viewerState.layers[layerId].images[imageId].dirty
+        ) {
+          // TODO delete image TiledImage
+          delete viewerState.layers[layerId].images[imageId];
+        }
+      }
+      if (!(layerId in layers) || viewerState.layers[layerId].dirty) {
+        // TODO delete dummy TiledImage
+        delete viewerState.layers[layerId];
+      }
+    }
+    for (const layerId of Object.keys(layers)) {
+      if (!(layerId in viewerState.layers)) {
+        const dummyTiledImageIndex = -1; // TODO create dummy TiledImage
+        viewerState.layers[layerId] = {
+          dummyTiledImageIndex: dummyTiledImageIndex,
+          dirty: false,
+          images: {},
+        };
+      }
+      for (const imageId of Object.keys(layers[layerId].images)) {
+        if (!(imageId in viewerState.layers[layerId].images)) {
+          const tiledImageIndex = -1; // TODO create image TiledImage
+          viewerState.layers[layerId].images[imageId] = {
+            tiledImageIndex: tiledImageIndex,
+            dirty: false,
+          };
+        }
+      }
+    }
     return viewerState;
   }
 
