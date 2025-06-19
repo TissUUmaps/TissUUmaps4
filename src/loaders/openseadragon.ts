6b5075e120fdd94dbbc667017dd963fcd5c1de8f
Jonas Windhager <jonas.windhager@scilifelab.se>
2025-06-19 11:52:35 +0200

    feat: implement OpenSeadragon image data loader and source model

--- src/loaders/openseadragon.ts
+++ src/loaders/openseadragon.ts
@@ -1,1 +1,22 @@
-// TODO OpenSeadragonImageDataLoader
+import { IImageData, IImageDataLoader } from "../data/image";
+import { IImageDataSourceModel } from "../models/image";
+
+export const OPENSEADRAGON_IMAGE_DATA_SOURCE = "openseadragon";
+
+export interface OpenSeadragonImageDataSourceModel
+  extends IImageDataSourceModel<typeof OPENSEADRAGON_IMAGE_DATA_SOURCE> {
+  tileSource: string;
+}
+
+export class OpenSeadragonImageDataLoader
+  implements IImageDataLoader<OpenSeadragonImageDataSourceModel>
+{
+  loadImage(
+    dataSource: OpenSeadragonImageDataSourceModel,
+  ): Promise<IImageData> {
+    const imageData: IImageData = {
+      tileSource: dataSource.tileSource,
+    };
+    return Promise.resolve(imageData);
+  }
+}
