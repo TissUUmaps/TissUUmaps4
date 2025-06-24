081b53fd20db6d3460c4cc9ef4cb4a2f7d99ceeb
Jonas Windhager <jonas.windhager@scilifelab.se>
2025-06-25 00:11:32 +0200

    refactor: remove unused table data loader code and related comments

--- src/stores/tableSlice.ts
+++ src/stores/tableSlice.ts
@@ -1,5 +1,5 @@
-import { ITableData, ITableDataLoader } from "../data/table";
-import { ITableDataSourceModel, ITableModel } from "../models/table";
+import { ITableData } from "../data/table";
+import { ITableModel } from "../models/table";
 import MapUtils from "../utils/MapUtils";
 import { BoundStoreStateCreator } from "./boundStore";
 
@@ -8,27 +8,24 @@ export type TableSlice = TableSliceState & TableSliceActions;
 export type TableSliceState = {
   tables: Map<string, ITableModel>;
   tableData: Map<string, ITableData>;
-  tableDataLoaders: Map<
-    string,
-    ITableDataLoader<ITableDataSourceModel<string>>
-  >;
+  // tableDataLoaders: Map<
+  //   string,
+  //   ITableDataLoader<ITableDataSourceModel<string>>
+  // >;
 };
 
 export type TableSliceActions = {
   setTable: (tableId: string, table: ITableModel, tableIndex?: number) => void;
-  loadTable: (tableId: string, table?: ITableModel) => Promise<ITableData>;
+  // loadTable: (tableId: string, table?: ITableModel) => Promise<ITableData>;
   deleteTable: (tableId: string) => void;
-  registerTableDataLoader: (
-    tableDataSourceType: string,
-    tableDataLoader: ITableDataLoader<ITableDataSourceModel<string>>,
-  ) => void;
-  unregisterTableDataLoader: (tableDataSourceType: string) => void;
+  // registerTableDataLoader: (
+  //   tableDataSourceType: string,
+  //   tableDataLoader: ITableDataLoader<ITableDataSourceModel<string>>,
+  // ) => void;
+  // unregisterTableDataLoader: (tableDataSourceType: string) => void;
 };
 
-export const createTableSlice: BoundStoreStateCreator<TableSlice> = (
-  set,
-  get,
-) => ({
+export const createTableSlice: BoundStoreStateCreator<TableSlice> = (set) => ({
   ...initialTableSliceState,
   setTable: (tableId, table, tableIndex) => {
     set((draft) => {
@@ -40,61 +37,61 @@ export const createTableSlice: BoundStoreStateCreator<TableSlice> = (
       );
     });
   },
-  loadTable: async (tableId, table) => {
-    const state = get();
-    if (state.tableData.has(tableId)) {
-      return state.tableData.get(tableId)!;
-    }
-    if (table === undefined) {
-      table = state.tables.get(tableId);
-      if (table === undefined) {
-        throw new Error(`No table found for ID: ${tableId}`);
-      }
-    }
-    const tableDataLoader = state.tableDataLoaders.get(table.dataSource.type);
-    if (tableDataLoader === undefined) {
-      throw new Error(
-        `No table data loader registered for table data source type: ${table.dataSource.type}`,
-      );
-    }
-    const tableData = await tableDataLoader.loadTable(table.dataSource);
-    set((draft) => {
-      draft.tableData.set(tableId, tableData);
-    });
-    return tableData;
-  },
+  // loadTable: async (tableId, table) => {
+  //   const state = get();
+  //   if (state.tableData.has(tableId)) {
+  //     return state.tableData.get(tableId)!;
+  //   }
+  //   if (table === undefined) {
+  //     table = state.tables.get(tableId);
+  //     if (table === undefined) {
+  //       throw new Error(`No table found for ID: ${tableId}`);
+  //     }
+  //   }
+  //   const tableDataLoader = state.tableDataLoaders.get(table.dataSource.type);
+  //   if (tableDataLoader === undefined) {
+  //     throw new Error(
+  //       `No table data loader registered for table data source type: ${table.dataSource.type}`,
+  //     );
+  //   }
+  //   const tableData = await tableDataLoader.loadTable(table.dataSource);
+  //   set((draft) => {
+  //     draft.tableData.set(tableId, tableData);
+  //   });
+  //   return tableData;
+  // },
   deleteTable: (tableId) => {
     set((draft) => {
       draft.tables.delete(tableId);
       draft.tableData.delete(tableId);
     });
   },
-  registerTableDataLoader: (tableDataSourceType, tableDataLoader) => {
-    set((draft) => {
-      if (draft.tableDataLoaders.has(tableDataSourceType)) {
-        console.warn(
-          `Table data loader was already registered for table data source type: ${tableDataSourceType}`,
-        );
-      }
-      draft.tableDataLoaders.set(tableDataSourceType, tableDataLoader);
-    });
-  },
-  unregisterTableDataLoader: (tableDataSourceType) => {
-    set((draft) => {
-      if (!draft.tableDataLoaders.delete(tableDataSourceType)) {
-        console.warn(
-          `No table data loader registered for table data source type: ${tableDataSourceType}`,
-        );
-      }
-    });
-  },
+  // registerTableDataLoader: (tableDataSourceType, tableDataLoader) => {
+  //   set((draft) => {
+  //     if (draft.tableDataLoaders.has(tableDataSourceType)) {
+  //       console.warn(
+  //         `Table data loader was already registered for table data source type: ${tableDataSourceType}`,
+  //       );
+  //     }
+  //     draft.tableDataLoaders.set(tableDataSourceType, tableDataLoader);
+  //   });
+  // },
+  // unregisterTableDataLoader: (tableDataSourceType) => {
+  //   set((draft) => {
+  //     if (!draft.tableDataLoaders.delete(tableDataSourceType)) {
+  //       console.warn(
+  //         `No table data loader registered for table data source type: ${tableDataSourceType}`,
+  //       );
+  //     }
+  //   });
+  // },
 });
 
 const initialTableSliceState: TableSliceState = {
   tables: new Map<string, ITableModel>(),
   tableData: new Map<string, ITableData>(),
-  tableDataLoaders: new Map<
-    string,
-    ITableDataLoader<ITableDataSourceModel<string>>
-  >(),
+  // tableDataLoaders: new Map<
+  //   string,
+  //   ITableDataLoader<ITableDataSourceModel<string>>
+  // >(),
 };
