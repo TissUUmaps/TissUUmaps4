3a63e4f4f385f34bb67b26f435d78855daa661d8
Jonas Windhager <jonas.windhager@scilifelab.se>
2025-07-18 14:53:03 +0200

    add validation for array length in fillSeq method

--- src/utils/ArrayUtils.ts
+++ src/utils/ArrayUtils.ts
@@ -2,6 +2,9 @@ import { TypedArray } from "../data/types";
 
 export default class ArrayUtils {
   static fillSeq<T extends TypedArray>(arr: T, seq: number[]): void {
+    if (arr.length < seq.length || arr.length % seq.length !== 0) {
+      throw new Error("Array length must be a multiple of sequence length");
+    }
     for (let i = 0; i < seq.length; i++) {
       arr[i] = seq[i];
     }
