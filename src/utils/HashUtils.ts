756d3375196ba5a9dbb80f754e2ccae651d6ba7d
Jonas Windhager <jonas.windhager@scilifelab.se>
2025-08-14 19:07:37 +0200

    HashUtils

---
+++ src/utils/HashUtils.ts
@@ -0,0 +1,9 @@
+export default class HashUtils {
+  static djb2(str: string): number {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = (hash << 5) - hash + str.charCodeAt(i);
+    }
+    return hash;
+  }
+}
