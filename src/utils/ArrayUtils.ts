79233c21a67690207cfc982a8952551a8bd370fa
Jonas Windhager <jonas.windhager@scilifelab.se>
2025-09-22 23:58:02 +0200

    add first unit test

--- src/utils/ArrayUtils.ts
+++ src/utils/ArrayUtils.ts
@@ -2,6 +2,9 @@ import { TypedArray } from "../data/types";
 
 export default class ArrayUtils {
   static fillSeq<T extends TypedArray>(arr: T, seq: number[]): void {
+    if (arr.length === 0) {
+      return;
+    }
     if (arr.length < seq.length || arr.length % seq.length !== 0) {
       throw new Error("Array length must be a multiple of sequence length");
     }
