6f49dae70a47908386931983a32b18c8a36dc7cb
Jonas Windhager <jonas.windhager@scilifelab.se>
2025-09-24 12:02:22 +0200

    fix and optimize transformations

--- src/utils/TransformUtils.ts
+++ src/utils/TransformUtils.ts
@@ -4,6 +4,7 @@ import { SimilarityTransform } from "../models/types";
 
 export default class TransformUtils {
   static fromMatrix(m: mat3): SimilarityTransform {
+    // gl-matrix, like OpenGL, uses column-major order.
     return {
       scale: Math.sqrt(m[0] * m[0] + m[1] * m[1]),
       rotation: (Math.atan2(m[1], m[0]) * 180) / Math.PI,
@@ -15,35 +16,29 @@ export default class TransformUtils {
     tf: Partial<SimilarityTransform>,
     rotationCenter?: { x: number; y: number },
   ): mat3 {
+    // gl-matrix, like OpenGL, uses pre-multiplied matrices,
+    // so we need to apply transformations in reverse order.
     const m = mat3.create();
-    if (tf.scale !== undefined) {
-      const s = mat3.fromScaling(mat3.create(), [tf.scale, tf.scale]);
-      mat3.multiply(m, s, m);
+    if (tf.translation !== undefined) {
+      mat3.translate(m, m, [tf.translation.x, tf.translation.y]);
     }
     if (rotationCenter !== undefined) {
-      const t = mat3.fromTranslation(mat3.create(), [
-        -rotationCenter.x * (tf.scale ?? 1),
-        -rotationCenter.y * (tf.scale ?? 1),
-      ]);
-      mat3.multiply(m, t, m);
-    }
-    if (tf.rotation !== undefined) {
-      const r = mat3.fromRotation(mat3.create(), (Math.PI * tf.rotation) / 180);
-      mat3.multiply(m, r, m);
-    }
-    if (rotationCenter !== undefined) {
-      const t = mat3.fromTranslation(mat3.create(), [
+      mat3.translate(m, m, [
         rotationCenter.x * (tf.scale ?? 1),
         rotationCenter.y * (tf.scale ?? 1),
       ]);
-      mat3.multiply(m, t, m);
     }
-    if (tf.translation !== undefined) {
-      const t = mat3.fromTranslation(mat3.create(), [
-        tf.translation.x,
-        tf.translation.y,
+    if (tf.rotation !== undefined) {
+      mat3.rotate(m, m, (Math.PI * tf.rotation) / 180);
+    }
+    if (rotationCenter !== undefined) {
+      mat3.translate(m, m, [
+        -rotationCenter.x * (tf.scale ?? 1),
+        -rotationCenter.y * (tf.scale ?? 1),
       ]);
-      mat3.multiply(m, t, m);
+    }
+    if (tf.scale !== undefined) {
+      mat3.scale(m, m, [tf.scale, tf.scale]);
     }
     return m;
   }
