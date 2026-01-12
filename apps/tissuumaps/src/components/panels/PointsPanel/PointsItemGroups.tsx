fdb421018a9214d5153292e452d4504b9f1583b8
Jonas Windhager <jonas@windhager.io>
2026-01-12 14:33:39 +0100

    add PointsPanel skeleton

---
+++ apps/tissuumaps/src/components/panels/PointsPanel/PointsItemGroups.tsx
@@ -0,0 +1,8 @@
+import type { Points } from "@tissuumaps/core";
+
+export function PointsItemGroups(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _props: { points: Points },
+) {
+  return <></>;
+}
---
+++ apps/tissuumaps/src/components/panels/PointsPanel/PointsItemSettings.tsx
@@ -0,0 +1,8 @@
+import type { Points } from "@tissuumaps/core";
+
+export function PointsItemSettings(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _props: { points: Points },
+) {
+  return <></>;
+}
