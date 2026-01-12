7ce0604540b9d843282e3dcae9e9209d1d075480
Jonas Windhager <jonas.windhager@scilifelab.se>
2026-01-12 14:41:19 +0100

    add PointsPanel skeleton (#47)
    * add PointsPanel skeleton
    
    * add todos

--- apps/tissuumaps/src/components/panels/PointsPanel/index.tsx
+++ apps/tissuumaps/src/components/panels/PointsPanel/index.tsx
@@ -1,4 +1,69 @@
-export function PointsPanel() {
-  // TODO
-  return <></>;
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { DragDropProvider } from "@dnd-kit/react";
+import { isSortable, useSortable } from "@dnd-kit/react/sortable";
+import { GripVertical } from "lucide-react";
+import type { HTMLProps } from "react";
+
+import type { Points } from "@tissuumaps/core";
+
+import { useTissUUmaps } from "../../../store";
+import { PointsItem } from "./PointsItem";
+
+export function PointsPanel(props: HTMLProps<HTMLDivElement>) {
+  const points = useTissUUmaps((state) => state.points);
+  const movePoints = useTissUUmaps((state) => state.movePoints);
+
+  return (
+    <div {...props}>
+      <DragDropProvider
+        onDragEnd={(event) => {
+          const { source, canceled } = event.operation;
+          if (isSortable(source) && !canceled) {
+            // dnd-kit optimistically updates the DOM
+            // https://github.com/clauderic/dnd-kit/issues/1564
+            movePoints(source.id as string, source.index);
+          }
+        }}
+      >
+        <Accordion multiple>
+          {points.map((currentPoints, index) => (
+            <SortablePointsItem
+              key={currentPoints.id}
+              points={currentPoints}
+              index={index}
+            />
+          ))}
+        </Accordion>
+      </DragDropProvider>
+    </div>
+  );
+}
+
+function SortablePointsItem({
+  points,
+  index,
+}: {
+  points: Points;
+  index: number;
+}) {
+  const { ref, handleRef } = useSortable({ id: points.id, index });
+
+  return (
+    <div ref={ref}>
+      <AccordionItem>
+        <AccordionTrigger>
+          <GripVertical ref={handleRef} />
+          {points.name}
+        </AccordionTrigger>
+        <AccordionContent>
+          <PointsItem points={points} />
+        </AccordionContent>
+      </AccordionItem>
+    </div>
+  );
 }
