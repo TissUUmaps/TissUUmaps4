3a30157ced87513323c792b5a0dd4224310ecf54
Jonas Windhager <jonas.windhager@scilifelab.se>
2026-01-12 14:41:19 +0100

    add PointsPanel skeleton (#47)
    * add PointsPanel skeleton
    
    * add todos

---
+++ apps/tissuumaps/src/components/panels/PointsPanel/PointsItemSettings.tsx
@@ -0,0 +1,9 @@
+import type { Points } from "@tissuumaps/core";
+
+export function PointsItemSettings(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _props: { points: Points },
+) {
+  // TODO
+  return <></>;
+}
