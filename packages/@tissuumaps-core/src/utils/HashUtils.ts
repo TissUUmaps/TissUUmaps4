f1a5b6526da7c74003f364c83a7a7e4e490e1c25
Jonas Windhager <jonas@windhager.io>
2026-01-30 18:34:37 +0100

    fix: djb2 hash function

--- packages/@tissuumaps-core/src/utils/HashUtils.ts
+++ packages/@tissuumaps-core/src/utils/HashUtils.ts
@@ -1,9 +1,10 @@
 export class HashUtils {
+  // http://www.cse.yorku.ca/~oz/hash.html
   static djb2(str: string): number {
-    let hash = 0;
+    let hash = 5381;
     for (let i = 0; i < str.length; i++) {
-      hash = (hash << 5) - hash + str.charCodeAt(i);
+      hash = (hash << 5) + hash + str.charCodeAt(i);
     }
-    return hash;
+    return hash >>> 0; // ensure non-negative
   }
 }
