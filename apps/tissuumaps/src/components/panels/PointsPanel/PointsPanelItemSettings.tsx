9370134d6f0339f5b44fe98328c402ef4a193f5e
Jonas Windhager <jonas@windhager.io>
2026-01-30 16:01:19 +0100

    fix: open config control details upon toggling source

--- apps/tissuumaps/src/components/panels/PointsPanel/PointsPanelItemSettings.tsx
+++ apps/tissuumaps/src/components/panels/PointsPanel/PointsPanelItemSettings.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   type Points,
   defaultPointColor,
@@ -24,11 +26,28 @@ import {
   SizeConfigSourceToggleGroup,
 } from "../../controls/SizeConfigControl";
 
+const ConfigControl = {
+  pointColor: "pointColor",
+  pointSize: "pointSize",
+} as const;
+
+type ConfigControl = (typeof ConfigControl)[keyof typeof ConfigControl];
+
 export function PointsPanelItemSettings({ points }: { points: Points }) {
+  const [expandedConfigControl, setExpandedConfigControl] =
+    useState<ConfigControl | null>(null);
+
   const updatePoints = useTissUUmaps((state) => state.updatePoints);
 
   return (
-    <Accordion>
+    <Accordion
+      value={[expandedConfigControl]}
+      onValueChange={(value) => {
+        if (value.length === 1) {
+          setExpandedConfigControl(value[0] as ConfigControl | null);
+        }
+      }}
+    >
       {/* Point color */}
       <ColorConfigContextProvider
         colorConfig={points.pointColor}
@@ -36,11 +55,14 @@ export function PointsPanelItemSettings({ points }: { points: Points }) {
         defaultSource="constant"
         defaultColor={defaultPointColor}
       >
-        <AccordionItem>
+        <AccordionItem value={ConfigControl.pointColor}>
           <AccordionHeader>
             <AccordionTriggerRightDownIcon />
             <AccordionTrigger>Color</AccordionTrigger>
-            <ColorConfigSourceToggleGroup className="ml-auto" />
+            <ColorConfigSourceToggleGroup
+              className="ml-auto"
+              onClick={() => setExpandedConfigControl(ConfigControl.pointColor)}
+            />
           </AccordionHeader>
           <AccordionPanel>
             <ColorConfigControl />
@@ -56,11 +78,14 @@ export function PointsPanelItemSettings({ points }: { points: Points }) {
         defaultSource="constant"
         defaultSize={defaultPointSize}
       >
-        <AccordionItem>
+        <AccordionItem value={ConfigControl.pointSize}>
           <AccordionHeader>
             <AccordionTriggerRightDownIcon />
             <AccordionTrigger>Size</AccordionTrigger>
-            <SizeConfigSourceToggleGroup className="ml-auto" />
+            <SizeConfigSourceToggleGroup
+              className="ml-auto"
+              onClick={() => setExpandedConfigControl(ConfigControl.pointSize)}
+            />
           </AccordionHeader>
           <AccordionPanel>
             <SizeConfigControl />
