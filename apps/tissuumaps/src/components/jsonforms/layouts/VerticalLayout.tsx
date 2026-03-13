9b4ad012a8d2800d3253065453c696018ca1d712
Jonas Windhager <jonas.windhager@scilifelab.se>
2026-03-13 01:11:09 +0100

    fix: update dependencies (except OpenSeadragon) (#83)
    fix: update dependencies (except OpenSeadragon)

--- apps/tissuumaps/src/components/jsonforms/layouts/VerticalLayout.tsx
+++ apps/tissuumaps/src/components/jsonforms/layouts/VerticalLayout.tsx
@@ -9,6 +9,7 @@ import {
 } from "@jsonforms/react";
 import React from "react";
 
+// eslint-disable-next-line react-refresh/only-export-components
 const MemoizedVerticalLayout = React.memo(
   (props: Omit<LayoutProps, "data">) => {
     const layout = props.uischema as VerticalLayoutSchema;
