3f3f4d3c06a7786ec8a79168713acbd71a715f2e
Christophe Avenel <christophe.avenel@it.uu.se>
2026-03-26 19:59:10 +0100

    fix: Add back comment

--- apps/tissuumaps/src/components/panels/ImagesPanel/index.tsx
+++ apps/tissuumaps/src/components/panels/ImagesPanel/index.tsx
@@ -37,6 +37,8 @@ export function ImagesPanel({ className }: ImagesPanelProps) {
       onDragEnd={(event) => {
         const { source, canceled } = event.operation;
         if (isSortable(source) && !canceled) {
+          // dnd-kit optimistically updates the DOM
+          // https://github.com/clauderic/dnd-kit/issues/1564
           moveImage(source.id as string, source.index);
         }
       }}
